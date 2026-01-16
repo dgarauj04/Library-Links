73ae614e577e96964c58667a82a38565d57f9355
dgarauj04 <douglasdab10@gmail.com>
2026-01-16 03:37:20 -0300

    Atualização de  pesquisa e correção de cors

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,15 @@
 import uvicorn;
+import os
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    try:
+        port = int(os.environ.get("CORS_ORIGINS", 8000))
+    except ValueError:
+        port = 8000
+        
     uvicorn.run(
         "src.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=True  
     )
